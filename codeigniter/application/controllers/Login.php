a2508e3a04be9f9fe2637f13e17996727f964d8c
Em <104334306+JohnM-Reyes24@users.noreply.github.com>
2022-08-07 19:42:01 +0800

    Update Login.php

--- codeigniter/application/controllers/Login.php
+++ codeigniter/application/controllers/Login.php
@@ -3,7 +3,8 @@
 class Login extends CI_Controller{
     public function __construct() {
         parent::__construct();
-
+    }
+        
     public function index() {
         $data['title'] = 'Calibr8 - LOGIN';
         $this->load->view('include/header.php', $data);
