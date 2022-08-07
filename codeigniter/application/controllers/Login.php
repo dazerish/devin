7bfd3a392d98a30669231b87c82d0c06627cfb6c
Em <104334306+JohnM-Reyes24@users.noreply.github.com>
2022-08-07 19:04:32 +0800

    Changes in Login.php controller

--- codeigniter/application/controllers/Login.php
+++ codeigniter/application/controllers/Login.php
@@ -1,11 +1,22 @@
 <?php 
 
 class Login extends CI_Controller{
+    public function __construct() {
+        parent::__construct();
 
-    public function login_view() {
-        $this->load->view('include/header.php');
+        $this->load->helper(['form','url']);
+        // $this->load->libary('session');
+    }
+
+    public function index() { //Login View
+        $data['title'] = 'Calibr8 - LOGIN';
+        $this->load->view('include/header.php', $data);
         $this->load->view('login_view');
         $this->load->view('include/footer.php');
+    }
+
+    public function logout() {
+        
     }
 
 }
