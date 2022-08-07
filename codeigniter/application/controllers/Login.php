7a20943ee8a043c6548390939ba8bd8db25d9c9f
Em <104334306+JohnM-Reyes24@users.noreply.github.com>
2022-08-07 19:05:01 +0800

    Revert "Changes in Login.php controller"
    This reverts commit 7bfd3a392d98a30669231b87c82d0c06627cfb6c.

--- codeigniter/application/controllers/Login.php
+++ codeigniter/application/controllers/Login.php
@@ -1,22 +1,11 @@
 <?php 
 
 class Login extends CI_Controller{
-    public function __construct() {
-        parent::__construct();
 
-        $this->load->helper(['form','url']);
-        // $this->load->libary('session');
-    }
-
-    public function index() { //Login View
-        $data['title'] = 'Calibr8 - LOGIN';
-        $this->load->view('include/header.php', $data);
+    public function login_view() {
+        $this->load->view('include/header.php');
         $this->load->view('login_view');
         $this->load->view('include/footer.php');
-    }
-
-    public function logout() {
-        
     }
 
 }
