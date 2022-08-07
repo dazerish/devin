93d128286dc5edd14fdcec1f0909a22d6644bce9
Em <104334306+JohnM-Reyes24@users.noreply.github.com>
2022-08-07 19:08:21 +0800

    Changes in controllers folder

--- codeigniter/application/controllers/Login.php
+++ codeigniter/application/controllers/Login.php
@@ -1,11 +1,18 @@
 <?php 
 
 class Login extends CI_Controller{
+    public function __construct() {
+        parent::__construct();
 
-    public function login_view() {
-        $this->load->view('include/header.php');
+    public function index() {
+        $data['title'] = 'Calibr8 - LOGIN';
+        $this->load->view('include/header.php', $data);
         $this->load->view('login_view');
         $this->load->view('include/footer.php');
+    }
+
+    public function logout() {
+        
     }
 
 }
