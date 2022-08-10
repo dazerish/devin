1aee02801fb1fa99f0c4b230fb251fc78b30d1d9
Elaine <80563158+dazerish@users.noreply.github.com>
2022-08-10 15:24:24 +0800

    added login view code

---
+++ codeigniter/application/views/login_view.php
@@ -0,0 +1,21 @@
+<div class="login_logo"><img src="assets/pictures/calibr8logo.jpg" alt="Calibr8 Logo" height="30px"></div>
+    
+    <section class="homepage">
+        
+            <div class="login_box">
+                <div class="login_container">
+                    <h3 class="login_header">Sign in your Calibr8 Account</h3>    
+                 <form action="#" class="login_form" method="#">
+                     <label for="email" class="login_label">Email</label><br>
+                     <input type="text" id="email" name="email"><br>
+                     <label for="password" class="login_label">Password</label><br>
+                     <input type="password" id="pword" name="pword">
+                </div>
+                <input type="submit" class="all_btn" id="login_btn" value="LOGIN">
+                </form>
+                    <div class="account_notice">
+                        <p>Forgot your password? <a href="#">Reset Password</a></p>
+                        <p>Don't have an account yet? <b>Contact the Admin Office.</b></p>
+                    </div>
+             </div>
+    </section>
