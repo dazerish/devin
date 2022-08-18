2e5640d0830696e04c4c82ee1afcb1cae7e21038
Em <104334306+JohnM-Reyes24@users.noreply.github.com>
2022-08-18 18:27:57 +0800

    Merged updated employee_register

--- codeigniter/application/views/admin_empReg_view.php
+++ codeigniter/application/views/admin_empReg_view.php
@@ -1,66 +1,79 @@
 <nav>
-        <div class="logo"><img src="<?= base_url('./assets/pictures/calibr8logo.jpg');?>" alt="Calibr8 Logo" height="30px"></div>
-        <a class="nav-link" href="#">Dashboard</a>
-        <a class="nav-link" href="#">View</a>
-        <a class="nav-link" href="#">Edit</a>
-        <a class="nav-link" href="#">Reservation</a>
-        <a class="nav-link" href="#">Generate Reports</a>
-        <div class="dropdown">
-            <a href="#" class="regbtn" id="activebtn">Registration</a>
-            <div class="list">
-                <a href="<?= site_url('Admin/devReg_view')?>" class="links">Device Registration</a>
-                <a href="<?= site_url('Admin/empReg_view')?>" class="links">Employee Registration</a>
-            </div>
+    <div class="logo"><img src="<?= base_url('./assets/pictures/calibr8logo.jpg');?>" alt="Calibr8 Logo" height="30px"></div>
+    <a class="nav-link" href="#">Dashboard</a>
+    <a class="nav-link" href="#">View</a>
+    <a class="nav-link" href="#">Edit</a>
+    <a class="nav-link" href="#">Reservation</a>
+    <a class="nav-link" href="#">Generate Reports</a>
+    <div class="dropdown">
+        <a href="#" class="regbtn" id="activebtn">Registration</a>
+        <div class="list">
+            <a href="<?= site_url('Admin/devReg_view')?>" class="links">Device Registration</a>
+            <a href="<?= site_url('Admin/empReg_view')?>" class="links">Employee Registration</a>
         </div>
-        <a class="nav-link" href="<?= site_url('Login/logout')?>">Logout</a> <!-- Temporary only -->
-
-        <a href="#" class="ts"><i class="far fa-user" id="nav-user-icon"></i>Admin</a>
-        </form>
-</nav>
-    <script>
-        let click = document.querySelector('.regbtn');
-        let list = document.querySelector('.list');
-        click.addEventListener("click", ()=> {
-            list.classList.toggle('newList');
-        });
-    </script>
-  
+    </div>
     
-    <section class="main_container">
+    <a href="#" class="ts"><i class="far fa-user" id="nav-user-icon"></i>Admin</a>
+    </form>
+</nav>
+<script>
+    let click = document.querySelector('.regbtn');
+    let list = document.querySelector('.list');
+    click.addEventListener("click", ()=> {
+        list.classList.toggle('newList');
+    });
+</script>
+
+
+<section class="main_container">
+    
         <div class="register_container">
             
             <div class="login_box">
                 <!-- FORM HERE -->
                 <form action="#" class="register_employee" method="#">
-                    <h3 class="login_header" style="text-align: center">Employee Registration</h3>
+                    <p class="login_header">Employee Registration</p>
+                <div class="row">
+                    <div class="col">
+                        <label for="empid" class="register_label">Employee ID</label><br>
+                        <input type="text" id="empid" name="empid"><br>
 
-                    <label for="empid" class="register_label">Employee ID</label><br>
-                    <input type="text" id="empid" name="empid"><br>
+                        <label for="empname" class="register_label">Employee Name</label><br>
+                        <input type="text" id="empname" name="empname"><br>
 
-                    <label for="empname" class="register_label">Employee Name</label><br>
-                    <input type="text" id="empname" name="empname"><br>
+                        <label for="superior" class="register_label">Direct Superior</label><br>
+                        <input type="text" id="superior" name="superior"><br>
+                    </div>
 
-                    <label for="superior" class="register_label">Direct Superior</label><br>
-                    <input type="text" id="superior" name="superior"><br>
+                    <div class="col">
+                        <label for="roles" class="register_label">Employee Role</label><br>
+                        <select name="roles" id="roles">    
+                            <option value="admin">Administrator</option>
+                            <option value="employee">Employee</option>
+                            <option value="exec">Executive</option>
+                        </select><br>
 
-                    <label for="roles" class="register_label">Employee Role</label><br>
-                    <select name="roles" id="roles">    
-                        <option value="admin">Administrator</option>
-                        <option value="employee">Employee</option>
-                        <option value="exec">Executive</option>
-                    </select><br>
+                        <label for="init-pass" class="register_label">Initial Password</label><br>
+                        <input type="text" id="superior" name="superior"><br>
 
-                    <label for="init-pass" class="register_label">Initial Password</label><br>
-                    <input type="text" id="superior" name="superior"><br>
-
-                    <label for="emp-img" class="register_label">Employee Image</label><br>
-                    <form action="/action_page.php">
-                    <input type="file" id="myFile" name="filename">
-                    </form>
-                        
-                    <input type="submit" class="all_btn" id="login_btn" value="REGISTER EMPLOYEE">
+                        <label for="emp-img" class="register_label">Employee Image</label><br>
+                        <form action="/action_page.php">
+                            <input type="file" id="upload" hidden/>
+                            <label for="upload" class="upload-btn">Upload image </label>
+                            <span id="file-chosen">No file chosen</span>
+                        </form>
+                    </div>
+                </div>
+                
+                
+                    
+                    
+                <div class="reg-div">
+                    <input type="submit" class="all_btn" id="reg-dev" value="REGISTER EMPLOYEE">
+                </div>
+                    
                 </form>
             </div>
-          
-        </div>   
-    </section>
+        
+        </div> 
+</section>
