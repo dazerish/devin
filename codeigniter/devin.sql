fc656489af5000ddac2c3caf841d3f7f7db1a6f3
Em <104334306+JohnM-Reyes24@users.noreply.github.com>
2022-08-24 23:24:36 +0800

    Pushed updated devin sql file

--- codeigniter/devin.sql
+++ codeigniter/devin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 18, 2022 at 02:56 PM
+-- Generation Time: Aug 24, 2022 at 05:11 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -24,10 +24,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `device_reg`
+-- Table structure for table `devices`
 --
 
-CREATE TABLE `device_reg` (
+CREATE TABLE `devices` (
   `id` int(11) NOT NULL,
   `unique_num` varchar(255) NOT NULL,
   `dev_name` varchar(50) NOT NULL,
@@ -39,12 +39,15 @@ CREATE TABLE `device_reg` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `device_reg`
+-- Dumping data for table `devices`
 --
 
-INSERT INTO `device_reg` (`id`, `unique_num`, `dev_name`, `dev_model`, `allowed_roles`, `manufacturer`, `specs`, `dev_image`) VALUES
-(1, '4CE0460D0G', 'Laptop', 'Legion 5', 'administrator', 'Lenovo', 'Ryzen 7 4800H\r\nGTX 1660 Ti\r\n16GB RAM\r\n512GB NVME', 'Lenovo-Legion1.png'),
-(2, '4CE0460D1X', 'Laptop', 'Omen 15', 'employee', 'HP', 'Ryzen 7 4800H\r\nGTX 1660 Ti\r\n16GB RAM\r\n512GB NVME', 'HP-Omen1.jpg');
+INSERT INTO `devices` (`id`, `unique_num`, `dev_name`, `dev_model`, `allowed_roles`, `manufacturer`, `specs`, `dev_image`) VALUES
+(1, '4CE0460D0G', 'Lenovo ThinkPad X1', 'Laptop', 'Administrator', 'Lenovo', 'Processor: Intel Core i7\r\nRAM: 16GB 3200MHz\r\nGraphics: Integrated Intel HD Graphics 4000\r\nHard Drive: 1TB\r\nDisplay: 14\" inch; 1920x1080 Resolution\r\nOS: Windows 10', 'Lenovo-ThinkPad-X1-1.png'),
+(2, '4CE0460D1X', 'Macbook Pro', 'Laptop', 'Executive', 'Apple', 'Processor: Apple M2 Chip\r\nRAM: 16GB Memory\r\nGraphics: Intel Integrated Iris Plus Graphics 645\r\nStorage: 512GB SSD\r\nDisplay: 13.3\" inch; 2560x1600 Resolution\r\nOS: macOS', 'Macbook-Pro-1.jpg'),
+(3, '4CE0460D69', 'Edge Gateway', 'Router', 'Employee', 'Dell', 'Processor: ARM Cortex-A53\r\nRAM: 2GB\r\nStorage: 512GB SSD\r\nOS: Linux Ubuntu\r\nWeight: 820g', 'Edge-Gateway1.png'),
+(4, '4CE0460D5G', 'Oculus Rift S', 'VR Headset', 'Executive', 'Oculus', 'Platform: SteamVR, Oculus Home\r\nDisplay Type: Single LCD (Binocular)\r\nResolution: 1280x1440 (per-eye)\r\nRefresh Rate: 80hz\r\nWeight: 500g', 'Occulus-Rift-S1.jpg'),
+(5, '4CE0460D1S', 'PowerEdge-T330', 'Server', 'Administrator', 'Dell', 'Processer: Intel Xeon\r\nRAM: 64GB\r\nDrive Bays: Supports 2.5\" HDD in 3.5\" hybrid drive \r\ncarrier\r\nOS: Windows Server', 'PowerEdge-T330-1.jpg');
 
 -- --------------------------------------------------------
 
@@ -68,7 +71,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `emp_id`, `emp_name`, `emp_email`, `superior`, `emp_role`, `password`, `emp_image`) VALUES
-(1, '2022-E003', 'John Michael Reyes', 'johnmichaelreyes24.ol@gmail.com', 'Jose Luis Reyes', 'employee', 'e9666c721b408f3706d066e8f28bca74', 'JM1.png');
+(1, '2022-HA001', 'Head Admin', 'admin@gmail.com', 'David Lim', 'administrator', '0192023a7bbd73250516f069df18b500', 'placeholder1.png'),
+(2, '2022-E0001', 'Andrea Blancaflor', 'andrea.blancaflor@gmail.com', 'Jose Luis Reyes', 'employee', 'ce6e92851b851beb843a47ce337bc1df', 'Andrea1.png'),
+(3, '2022-E0002', 'Rovic Gabriel Tabucol', 'rovic.tabucol@gmail.com', 'Juan Dela Cruz', 'employee', '4c4501548cb1fbf146201d6b3f1828b1', 'Rovic1.jpg'),
+(4, '2022-E0003', 'Elaine Enricoso', 'elaine.enricoso@gmail.com', 'Jane Doe', 'employee', '9c1533d53b02039a76402407d65a5e46', 'Elaine1.png'),
+(5, '2022-E0004', 'John Michael Reyes', 'jeem@gmail.com', 'Enteng Kabisote', 'employee', '11edd5475736938f90e47017c3c51b10', 'JM1.png');
 
 -- --------------------------------------------------------
 
@@ -97,9 +104,9 @@ INSERT INTO `user_information` (`id`, `first_name`, `last_name`, `email`, `passw
 --
 
 --
--- Indexes for table `device_reg`
+-- Indexes for table `devices`
 --
-ALTER TABLE `device_reg`
+ALTER TABLE `devices`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -119,16 +126,16 @@ ALTER TABLE `user_information`
 --
 
 --
--- AUTO_INCREMENT for table `device_reg`
+-- AUTO_INCREMENT for table `devices`
 --
-ALTER TABLE `device_reg`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `devices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user_information`
