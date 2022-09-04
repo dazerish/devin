14c30395f41b0d79b6918f33085e2214ebc9a05c
Em <104334306+JohnM-Reyes24@users.noreply.github.com>
2022-09-04 18:24:23 +0800

    Updated employee controller

--- codeigniter/application/controllers/Employee.php
+++ codeigniter/application/controllers/Employee.php
@@ -4,8 +4,8 @@ class Employee extends CI_Controller{
     public function __construct() {
         parent::__construct();
 
-        $this->load->helper(['form', 'url']);
-        $this->load-library('session');
+        $this->load->helper(['form', 'url', 'string']);
+        $this->load->library(['form_validation', 'session', 'pagination',]);
     }
 
     public function index() {
@@ -13,15 +13,99 @@ class Employee extends CI_Controller{
             redirect('Login');
         }
 
-        if ($this->session->userdata('role') == 'admin') {
+        if ($this->session->userdata('role') == 'admininistrator') {
             redirect('Admin');
         }
 
-        // Not yet done
-        // $data['title'] = 'Calibr8 - Employee Registration';
-        // $this->load->view('include/header', $data);
-        // $this->load->view('admin_empReg_view');
-        // $this->load->view('include/footer');
+        $data['title'] = 'Calibr8 - Device Reservation';
+        $this->load->view('include/header', $data);
+        $this->load->view('Employee_borrowDev_view'); //Temporary view to be loaded
+        $this->load->view('include/footer');
+    }
+
+    public function devList_view() {
+        $page_config = array(
+            'base_url' => site_url('Employee/employee_borrowDev_view'),
+            'total_rows' => $this->Employee_model->get_dCount(),
+            'num_links' => 3,
+            'per_page' => 5,
+
+            'full_tag_open' => '<div class="d-flex justify-content-center"><ul class="pagination">',
+            'full_tag_close' => '</ul></div>',
+
+            'first_link' => FALSE,
+            'last_link' => FALSE,
+
+            'next_link' => '&rsaquo;',
+            'next_tag_open' => '<li class="page-item">',
+            'next_tag_close' => '</li>',
+
+            'prev_link' => '&lsaquo;',
+            'prev_tag_open' => '<li class="page-item">',
+            'prev_tag_close' => '</li>',
+
+            'cur_tag_open' => '<li class="page-item active"><span class="page-link">',
+            'cur_tag_close' => '</span></li>',
+
+            'num_tag_open' => '<li class="page-item">',
+            'num_tag_close' => '</li>',
+
+            'attributes' => ['class' => 'page-link']
+        );
+
+        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+        $this->pagination->initialize($page_config);
+
+        $data['title'] = 'Calibr8 - Device Masterlist';
+        $data['devices'] = $this->Employee_model->get_devices_table($page_config['per_page'], $page);
+        $data['total'] = $this->Employee_model->get_dCount();
+        $this->load->view('include/header', $data);
+        $this->load->view('employee_borrowDev_view');
+        $this->load->view('include/footer');
+    }
+
+    public function searchDev() { //Temporary Search Function
+        $search = ($this->input->post("searchTerm")) ? $this->input->post("searchTerm") : "NIL";
+        $search = ($this->uri->segment(3)) ? $this->uri->segment(3) : $search;
+
+        $page_config = array(
+            'base_url' => site_url('Admin/searchDev/$search'),
+            'total_rows' => $this->Employee_model->get_devices_count($search),
+            'num_links' => 3,
+            'per_page' => 5,
+
+            'full_tag_open' => '<div class="d-flex justify-content-center"><ul class="pagination">',
+            'full_tag_close' => '</ul></div>',
+
+            'first_link' => FALSE,
+            'last_link' => FALSE,
+
+            'next_link' => '&rsaquo;',
+            'next_tag_open' => '<li class="page-item">',
+            'next_tag_close' => '</li>',
+
+            'prev_link' => '&lsaquo;',
+            'prev_tag_open' => '<li class="page-item">',
+            'prev_tag_close' => '</li>',
+
+            'cur_tag_open' => '<li class="page-item active"><span class="page-link">',
+            'cur_tag_close' => '</span></li>',
+
+            'num_tag_open' => '<li class="page-item">',
+            'num_tag_close' => '</li>',
+
+            'attributes' => ['class' => 'page-link']
+        );
+
+        $page = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0;
+        $this->pagination->initialize($page_config);
+
+        $data['title'] = 'Calibr8 - Employee Masterlist';
+        $data['devices'] = $this->Employee_model->get_devices_table($page_config['per_page'], $page, $search);
+        $data['total'] = $this->Employee_model->get_dCount();
+        $this->load->view('include/header', $data);
+        $this->load->view('employee_borrowDev_view');
+        $this->load->view('include/footer');
     }
 }
 
