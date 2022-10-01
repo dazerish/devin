3973555a3216a6b90f2b9f28593fbb30818bfed0
JL <joseluisreyes43@gmail.com>
2022-10-01 15:42:09 +0800

    Revert "Added model->controller->view for dashboard data (device in, device out, reserved, broken, in maintenance)"
    This reverts commit b2952b765c3a13653ac55ba30f80992018c0fe8f.

--- codeigniter/application/views/admin_dashboard_view.php
+++ codeigniter/application/views/admin_dashboard_view.php
@@ -3,27 +3,17 @@
     <div class="detail-container">
         <script>
             // Create chart instance
-            var chart = am4core.create("device_types_pie_div", am4charts.PieChart);
+            var chart = am4core.create("chartdiv", am4charts.PieChart);
 
             // passing the dashboard data from php to javascript for manipulation and display
-            var pie_data = <?php echo json_encode($dashboard_results[0]); ?>;
-
+            var dashboard_data = <?php echo json_encode($dashboard_results); ?>;
             console.log(dashboard_data)
-
-            // Pie Chart
-
-            chart.data = pie_data;
+            chart.data = dashboard_data;
 
             var pieSeries = chart.series.push(new am4charts.PieSeries());
             pieSeries.dataFields.value = "device_count";
             pieSeries.dataFields.category = "dev_model";
         </script>
-        <div id="device_types_pie_div" style="width: 100%; height: 400px;"></div>
-        <div id="device_in_div">Device In: <?= json_encode($dashboard_results[1])[0]->device_count; ?></div>
-        <div id="device_out_div">Device Out: <?= json_encode($dashboard_results[2])[0]->device_count; ?></div>
-        <div id="reserved_div">Reserved: <?= json_encode($dashboard_results[3])[0]->device_count; ?></div>
-        <div id="broken_div">Broken: <?= json_encode($dashboard_results[4])[0]->device_count; ?></div>
-        <div id="maintenance_div">In Maintenance: <?= json_encode($dashboard_results[5])[0]->device_count; ?></div>
+        <div id="chartdiv" style="width: 100%; height: 400px;"></div>
     </div>
-
 </div>
