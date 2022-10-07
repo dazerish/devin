ea9f4360c33b16e2d0fb489d9d2e1f5089179fd3
JL Reyes <joseluisreyes43@gmail.com>
2022-10-07 23:37:13 +0800

    edited arduino code for 2 instances of rfid. edited admin_empReg_view to incorporate arduino (not done)

--- codeigniter/application/views/admin_empReg_view.php
+++ codeigniter/application/views/admin_empReg_view.php
@@ -61,4 +61,325 @@
         </div>
     
     </div> 
+
+
+<!-- from rfidattendance -->
+  	<link rel = "icon" href ="css/assets/c8.jpg" type = "image/x-icon">
+	<link rel="stylesheet" type="text/css" href="css/manageusers.css">
+
+    <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.js"
+	        integrity="sha1256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+	        crossorigin="anonymous">
+	</script>
+	<script>
+	  	$(window).on("load resize ", function() {
+		    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+		    $('.tbl-header').css({'padding-right':scrollWidth});
+		}).resize();
+	</script>
+	<script>
+	  $(document).ready(function(){
+	  	  $.ajax({
+	        url: "manage_users_up.php"
+	        }).done(function(data) {
+	        $('#manage_users').html(data);
+	      });
+	    setInterval(function(){
+	      $.ajax({
+	        url: "manage_users_up.php"
+	        }).done(function(data) {
+	        $('#manage_users').html(data);
+	      });
+	    },5000);
+	  });
+	</script>
+	<!-- <h1 class="slideInDown animated">MANAGE USERS</h1> -->
+	<div>
+		<form enctype="multipart/form-data">
+			<div class="alert_user"></div>
+			<fieldset>
+				<legend><span class="number">1</span> User Info</legend>
+				<input type="hidden" name="user_id" id="user_id">
+				<input type="text" name="name" id="name" placeholder="Username">
+				<input type="text" name="number" id="number" placeholder="Serial Number">
+				<input type="email" name="email" id="email" placeholder="Email">
+			</fieldset>
+			<fieldset>
+			<legend><span class="number">2</span> Additional Info</legend>
+			<label>
+				<label for="Device"><b>User Department:</b></label>
+                    <select class="dev_sel" name="dev_sel" id="dev_sel" style="color: #000;">
+                      <option value="0">All Departments</option>
+                      <?php
+                        require'connectDB.php';
+                        $sql = "SELECT * FROM devices ORDER BY device_name ASC";
+                        $result = mysqli_stmt_init($conn);
+                        if (!mysqli_stmt_prepare($result, $sql)) {
+                            echo '<p class="error">SQL Error</p>';
+                        } 
+                        else{
+                            mysqli_stmt_execute($result);
+                            $resultl = mysqli_stmt_get_result($result);
+                            while ($row = mysqli_fetch_assoc($resultl)){
+                      ?>
+                              <option value="<?php echo $row['device_uid'];?>"><?php echo $row['device_dep']; ?></option>
+                      <?php
+                            }
+                        }
+                      ?>
+                    </select>
+				<input type="radio" name="gender" class="gender" value="Female"> Female
+	          	<input type="radio" name="gender" class="gender" value="Male" checked="checked"> Male
+	      	</label >
+			</fieldset>
+			<button type="button" name="user_add" class="user_add">Add User</button>
+			<button type="button" name="user_upd" class="user_upd">Update User</button>
+			<button type="button" name="user_rmo" class="user_rmo">Remove User</button>
+		</form>
+	</div>
+
+	<!--User table-->
+	<div class="section">
+		
+		<div class="slideInRight animated">
+			<div id="manage_users"></div>
+		</div>
+	</div>
 </section>
+<script>
+    $(document).ready(function(){
+      // Add user
+      $(document).on('click', '.user_add', function(){
+        //user Info
+        var user_id = $('#user_id').val();
+        var name = $('#name').val();
+        var number = $('#number').val();
+        var email = $('#email').val();
+        //Additional Info
+        var dev_uid = $('#dev_uid').val();
+        var gender = $(".gender:checked").val();
+        var dev_uid = $('#dev_sel option:selected').val();
+        
+        $.ajax({
+          url: 'manage_users_conf.php',
+          type: 'POST',
+          data: {
+            'Add': 1,
+            'user_id': user_id,
+            'name': name,
+            'number': number,
+            'email': email,
+            'dev_uid': dev_uid,
+            'gender': gender,
+          },
+          success: function(response){
+    
+            if (response == 1) {
+              $('#user_id').val('');
+              $('#name').val('');
+              $('#number').val('');
+              $('#email').val('');
+    
+              $('#dev_sel').val('0');
+              $('.alert_user').fadeIn(500);
+              $('.alert_user').html('<p class="alert alert-success">A new User has been successfully added</p>');
+            }
+            else{
+              $('.alert_user').fadeIn(500);
+              $('.alert_user').html('<p class="alert alert-danger">'+ response + '</p>');
+            }
+    
+            setTimeout(function () {
+                $('.alert').fadeOut(500);
+            }, 5000);
+            
+            $.ajax({
+              url: "manage_users_up.php"
+              }).done(function(data) {
+              $('#manage_users').html(data);
+            });
+          }
+        });
+      });
+      // Update user
+      $(document).on('click', '.user_upd', function(){
+        //user Info
+        var user_id = $('#user_id').val();
+        var name = $('#name').val();
+        var number = $('#number').val();
+        var email = $('#email').val();
+        //Additional Info
+        var dev_uid = $('#dev_uid').val();
+        var gender = $(".gender:checked").val();
+        var dev_uid = $('#dev_sel option:selected').val();
+    
+        $.ajax({
+          url: 'manage_users_conf.php',
+          type: 'POST',
+          data: {
+            'Update': 1,
+            'user_id': user_id,
+            'name': name,
+            'number': number,
+            'email': email,
+            'dev_uid': dev_uid,
+            'gender': gender,
+          },
+          success: function(response){
+    
+            if (response == 1) {
+              $('#user_id').val('');
+              $('#name').val('');
+              $('#number').val('');
+              $('#email').val('');
+    
+              $('#dev_sel').val('0');
+              $('.alert_user').fadeIn(500);
+              $('.alert_user').html('<p class="alert alert-success">The selected User has been updated!</p>');
+            }
+            else{
+              $('.alert_user').fadeIn(500);
+              $('.alert_user').html('<p class="alert alert-danger">'+ response + '</p>');
+            }
+            
+            setTimeout(function () {
+                $('.alert').fadeOut(500);
+            }, 5000);
+            
+            $.ajax({
+              url: "manage_users_up.php"
+              }).done(function(data) {
+              $('#manage_users').html(data);
+            });
+          }
+        });   
+      });
+      // delete user
+      $(document).on('click', '.user_rmo', function(){
+    
+        var user_id = $('#user_id').val();
+    
+        bootbox.confirm("Do you really want to delete this User?", function(result) {
+          if(result){
+            $.ajax({
+              url: 'manage_users_conf.php',
+              type: 'POST',
+              data: {
+                'delete': 1,
+                'user_id': user_id,
+              },
+              success: function(response){
+    
+                if (response == 1) {
+                  $('#user_id').val('');
+                  $('#name').val('');
+                  $('#number').val('');
+                  $('#email').val('');
+    
+                  $('#dev_sel').val('0');
+                  $('.alert_user').fadeIn(500);
+                  $('.alert_user').html('<p class="alert alert-success">The selected User has been deleted!</p>');
+                }
+                else{
+                  $('.alert_user').fadeIn(500);
+                  $('.alert_user').html('<p class="alert alert-danger">'+ response + '</p>');
+                }
+                
+                setTimeout(function () {
+                    $('.alert').fadeOut(500);
+                }, 5000);
+                
+                $.ajax({
+                  url: "manage_users_up.php"
+                  }).done(function(data) {
+                  $('#manage_users').html(data);
+                });
+              }
+            });
+          }
+        });
+      });
+      // select user
+      $(document).on('click', '.select_btn', function(){
+        var el = this;
+        var card_uid = $(this).attr("id");
+        $.ajax({
+          url: 'manage_users_conf.php',
+          type: 'GET',
+          data: {
+          'select': 1,
+          'card_uid': card_uid,
+          },
+          success: function(response){
+    
+            $(el).closest('tr').css('background','#70c276');
+    
+            $('.alert_user').fadeIn(500);
+            $('.alert_user').html('<p class="alert alert-success">The card has been selected!</p>');
+            
+            setTimeout(function () {
+                $('.alert').fadeOut(500);
+            }, 5000);
+    
+            $.ajax({
+              url: "manage_users_up.php"
+              }).done(function(data) {
+              $('#manage_users').html(data);
+            });
+    
+            console.log(response);
+    
+            var user_id = {
+              User_id : []
+            };
+            var user_name = {
+              User_name : []
+            };
+            var user_on = {
+              User_on : []
+            };
+            var user_email = {
+              User_email : []
+            };
+            var user_dev = {
+              User_dev : []
+            };
+            var user_gender = {
+              User_gender : []
+            };
+    
+            var len = response.length;
+    
+            for (var i = 0; i < len; i++) {
+                user_id.User_id.push(response[i].id);
+                user_name.User_name.push(response[i].username);
+                user_on.User_on.push(response[i].serialnumber);
+                user_email.User_email.push(response[i].email);
+                user_dev.User_dev.push(response[i].device_uid);
+                user_gender.User_gender.push(response[i].gender);
+            }
+            if (user_dev.User_dev == "All") {
+              user_dev.User_dev = 0;
+            }
+            $('#user_id').val(user_id.User_id);
+            $('#name').val(user_name.User_name);
+            $('#number').val(user_on.User_on);
+            $('#email').val(user_email.User_email);
+            $('#dev_sel').val(user_dev.User_dev);
+    
+            if (user_gender.User_gender == 'Female'){
+                $('.form-style-5').find(':radio[name=gender][value="Female"]').prop('checked', true);
+            }
+            else{
+                $('.form-style-5').find(':radio[name=gender][value="Male"]').prop('checked', true);
+            }
+    
+          },
+          error : function(data) {
+            console.log(data);
+          }
+        });
+      });
+    });
+</script>
