01367f29e6eb43a8cbb46412751a1dc7bc5bcfad
Elaine <80563158+dazerish@users.noreply.github.com>
2022-10-30 02:58:03 +0800

    responsive login view

--- codeigniter/application/views/login_view.php
+++ codeigniter/application/views/login_view.php
@@ -1,6 +1,9 @@
-<div class="login_logo">
-    <img src="<?= base_url('./assets/pictures/calibr8logo.png');?>" alt="Calibr8 Logo" height="30px">
-</div>
+<section class="logo-section">
+    <div class="login_logo">
+        <img src="<?= base_url('./assets/pictures/calibr8logo.png');?>" alt="Calibr8 Logo" height="30px">
+    </div>
+</section>
+
     
     <section class="homepage">
         
@@ -17,18 +20,17 @@
                         <?php endif; ?>
 
                         <label for="email" class="login_label">Email</label><br>
-                        <input type="text" id="email" name="email"><br>
+                        <input class="login-form" type="text" id="email" name="email"><br>
                         <label for="password" class="login_label">Password</label><br>
-                        <input type="password" id="pword" name="password">
+                        <input class="login-form" type="password" id="pword" name="password"><br>
                 
                         <input type="submit" class="all_btn" id="login_btn" value="LOGIN" name="login">
-                    
-                        
+                   
                         <?= form_close(); ?>
                         <div class="account_notice">
-                            <p>Forgot your password? <a href="#">Reset Password</a></p>
+                            <p>Forgot your password? <a href="#"><u>Reset Password</u></a></p>
                             <p>Don't have an account yet? <b>Contact the Admin Office.</b></p>
                         </div>
-                        
+                </div>        
             </div>
     </section>
