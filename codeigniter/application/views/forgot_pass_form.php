453666473d4fc1c8ee26fe84bb243b04a34c41b6
Elaine <80563158+dazerish@users.noreply.github.com>
2022-10-29 19:53:29 +0800

    added forgot password form

---
+++ codeigniter/application/views/forgot_pass_form.php
@@ -0,0 +1,18 @@
+<section class="homepage">
+    
+    <div class="login_box">
+        <div class="login_container">
+                <form>
+                <input type="button" class="back-btn" value="< BACK" onclick="history.back()">
+                </form>
+
+            <h3 class="login_header" style="text-align: center;">Forgot Password</h3>    
+    
+                <p class="reset-head" style="font-size: 20px; margin-bottom: 10px;">Enter your email address</p>
+                <label for="email" class="login_label">Email</label><br>
+                <input type="text" id="email" name="email"><br>
+    
+                    
+                <input type="submit" class="all_btn" id="reset_btn" value="Reset Password" name="reset-pw"> 
+        </div>
+</section>
